d560dfb6b7800817d4583ac6d7e7244748862024
ofazzito <ofazzito@yahoo.com.ar>
2020-10-24 20:55:56 -0300

    Agrego selector de temas por color

--- js/menu.js
+++ js/menu.js
@@ -1,3 +1,23 @@
+const PARAGRAPH = document.querySelector('body');
+const BUTTONS = document.querySelectorAll('.data-theme button');
+
+if (localStorage.getItem('demo-theme')) {
+	const theme = localStorage.getItem('demo-theme');
+	PARAGRAPH.classList.add(`theme-${theme}`);
+}
+
+BUTTONS.forEach(button => {
+  button.addEventListener('click', e => {
+    let colour = button.getAttribute('data-theme');
+    PARAGRAPH.className = '';
+    PARAGRAPH.classList.add(`theme-${colour}`);
+    localStorage.setItem('demo-theme', colour);
+  });
+});
+
+
+
+
 const activeMenu = () => {
   const openMenuButton = document.getElementById('open-menu-button')
   const closeMenuButton = document.getElementById('close-menu-button')
