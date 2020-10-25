ac70833d127976dbe92dc2ea937f5932b94663a8
ofazzito <ofazzito@yahoo.com.ar>
2020-10-25 02:43:45 -0300

    agrego boton go-top y fix varios

--- js/menu.js
+++ js/menu.js
@@ -1,21 +1,3 @@
-const PARAGRAPH = document.querySelector('body');
-const BUTTONS = document.querySelectorAll('.data-theme button');
-
-if (localStorage.getItem('demo-theme')) {
-	const theme = localStorage.getItem('demo-theme');
-	PARAGRAPH.classList.add(`theme-${theme}`);
-}
-
-BUTTONS.forEach(button => {
-  button.addEventListener('click', e => {
-    let colour = button.getAttribute('data-theme');
-    PARAGRAPH.className = '';
-    PARAGRAPH.classList.add(`theme-${colour}`);
-    localStorage.setItem('demo-theme', colour);
-  });
-});
-
-
 
 
 const activeMenu = () => {
@@ -33,3 +15,39 @@ const activeMenu = () => {
 }
 
 activeMenu()
+
+
+const PARAGRAPH = document.querySelector('body');
+const BUTTONS = document.querySelectorAll('.data-theme button');
+
+if (localStorage.getItem('demo-theme')) {
+	const theme = localStorage.getItem('demo-theme');
+	PARAGRAPH.classList.add(`theme-${theme}`);
+}
+
+BUTTONS.forEach(button => {
+  button.addEventListener('click', e => {
+    let colour = button.getAttribute('data-theme');
+    PARAGRAPH.className = '';
+    PARAGRAPH.classList.add(`theme-${colour}`);
+    localStorage.setItem('demo-theme', colour);
+  });
+});
+
+window.onscroll = function(){
+  if(document.documentElement.scrollTop > 400){
+    document.querySelector('.go-top-container')
+    .classList.add('show')
+  }else{
+    document.querySelector('.go-top-container')
+    .classList.remove('show')
+  }
+}
+
+document.querySelector('.go-top-container')
+.addEventListener('click',()=>{
+  window.scrollTo({
+    top:0,
+    behavior: 'smooth'
+  })
+})
